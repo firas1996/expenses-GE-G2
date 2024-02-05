5fbe2c3686e0ae5ee8b0ec5df7df7536db87ee5d
firas hamila <firas.hm.1996@gmail.com>
2024-02-05 11:47:48 +0100

    ss

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -1,14 +1,14 @@
-import React from "react";
+import "./ExpenseDate.css";
 
 const ExpenseDate = ({ date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("ar-Tn", { month: "long" });
   const day = date.toLocaleString("ar-EG", { day: "2-digit" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, price, date }) => {
+  const [myTitle, setMyTitle] = useState(title);
+  const changeHandler = () => {
+    // myTitle = "";
+    setMyTitle("New Title !!!!");
+    console.log(myTitle);
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{myTitle}</h2>
+        <button onClick={changeHandler}>Update Title !!!</button>
         <div className="expense-item__price"> $ {price}</div>
       </div>
     </div>
