22226ac5d70a82b483c5eef9a36a942a0786d63a
firas hamila <firas.hm.1996@gmail.com>
2024-02-12 11:40:50 +0100

    wcw

--- src/Components/NewExpenses/AddNewExpense.js
+++ src/Components/NewExpenses/AddNewExpense.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddNewExpense.css";
 
 const AddNewExpense = () => {
+  //   const [title, setTitle] = useState("");
+  //   const [price, setPrice] = useState("");
+  //   const [date, setDate] = useState("");
+  const [inputs, setInputs] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
+  const titleChanger = (event) => {
+    // setTitle((prevState) => {
+    //   return event.target.value;
+    // });
+    setInputs((prevState) => {
+      return { ...prevState, title: event.target.value };
+    });
+  };
+  const priceChanger = (event) => {
+    // setPrice(event.target.value);
+    // setInputs({ ...inputs, price: event.target.value });
+    setInputs((prevState) => {
+      return { ...prevState, price: event.target.value };
+    });
+  };
+  const dateChanger = (event) => {
+    // setDate(event.target.value);
+    // setInputs({ ...inputs, date: event.target.value });
+    setInputs((prevState) => {
+      return { ...prevState, date: event.target.value };
+    });
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // console.log(title);
+    // console.log(price);
+    // console.log(date);
+    console.log(inputs);
+    setInputs({
+      title: "",
+      price: "",
+      date: "",
+    });
+  };
   return (
     <div className="new-expense">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input required placeholder="Title" />
+            <input
+              required
+              placeholder="Title"
+              onChange={titleChanger}
+              value={inputs.title}
+            />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
@@ -18,16 +65,25 @@ const AddNewExpense = () => {
               min="0"
               step="0.01"
               placeholder="Price"
+              onChange={priceChanger}
+              value={inputs.price}
             />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input required type="date" min="2022-01-01" max="2026-12-31" />
+            <input
+              required
+              onChange={dateChanger}
+              value={inputs.date}
+              type="date"
+              min="2022-01-01"
+              max="2026-12-31"
+            />
           </div>
         </div>
         <div className="new-expense__actions">
           <button>Cancel</button>
-          <button>Add Expense</button>
+          <button type="submit">Add Expense</button>
         </div>
       </form>
     </div>
