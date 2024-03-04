97f956253d01134a6b4960383dd21753991e3425
firas hamila <firas.hm.1996@gmail.com>
2024-03-04 09:02:05 +0100

    hj

---
+++ src/Components/Expenses/ExpensesFilter.js
@@ -0,0 +1,12 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = () => {
+  return (
+    <div className="expenses-filter">
+      <h3>Filer By Year</h3>
+      <select></select>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
